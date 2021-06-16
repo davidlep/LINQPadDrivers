ff6f06c3b1645f20d1fe7a7865141a80b7c06c9b
David <davidlep92_@hotmail.com>
2021-06-15 20:23:40 -0400

    New connection dialog POC

---
+++ LINQPadDrivers.SimpleCSVDriver/NewConnectionDialog.xaml.cs
@@ -0,0 +1,37 @@
+using System.Windows;
+
+using LINQPad.Extensibility.DataContext;
+using Microsoft.Win32;
+
+namespace Davidlep.LINQPadDrivers.SimpleCsvDriver
+{
+    public partial class ConnectionDialog : Window
+	{
+		readonly IConnectionInfo connectionInfo;
+
+		public ConnectionDialog(IConnectionInfo connectionInfo)
+		{
+			this.connectionInfo = connectionInfo;
+			DataContext = new ConnectionProperties(connectionInfo);
+			InitializeComponent();
+		}
+
+		void BtnOK_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+		}
+
+		private void Browser_Click(object sender, RoutedEventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog
+			{
+				Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*"
+			};
+
+			if (openFileDialog.ShowDialog() == true)
+			{
+				(DataContext as ConnectionProperties).FilePath = openFileDialog.FileName;
+			}
+		}
+	}
+}
