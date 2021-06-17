ca614281dffd08910d0ee0c049f5696909bb57a0
David <davidlep92_@hotmail.com>
2021-06-16 21:33:02 -0400

    IDataProvider wip

---
+++ LINQPadDrivers.SimpleCSVDriver/IDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Davidlep.LINQPadDrivers.SimpleCsvDriver
+{
+    public interface IDataProvider<TRecord>
+    {
+        IEnumerable<TRecord> GetRecordsFromFile(string filepath);
+        IEnumerable<TRecord> GetRecordsFromFolder(string folderpath);
+        IEnumerable<TRecord> GetRecordsFromText(string text); //Pour le Clipboard
+        IEnumerable<TRecord> GetRecordsFromURL(Uri uri);
+    }
+}
